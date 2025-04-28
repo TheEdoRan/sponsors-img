20d56d3e993e2dd291443f248b555eb82caecc73
Edoardo Ranghieri <edoardo@ranghieri.com>
2025-04-28 15:53:29 +0200

    init

---
+++ sponsorkit.config.js
@@ -0,0 +1,46 @@
+import { defineConfig, tierPresets } from "sponsorkit";
+
+export default defineConfig({
+	// Providers configs
+	github: {
+		login: "TheEdoRan",
+		type: "user",
+	},
+	// Rendering configs
+	width: 800,
+	renderer: "tiers", // or 'circles'
+	formats: ["svg", "png"],
+	tiers: [
+		// Past sponsors, currently only supports GitHub
+		{
+			title: "Past Sponsors",
+			monthlyDollars: -1,
+			preset: tierPresets.xs,
+		},
+		// Default tier
+		{
+			title: "Supporters",
+			preset: tierPresets.base,
+		},
+		{
+			title: "Backers",
+			monthlyDollars: 10,
+			preset: tierPresets.medium,
+		},
+		{
+			title: "Entusiasts (Bronze)",
+			monthlyDollars: 25,
+			preset: tierPresets.large,
+		},
+		{
+			title: "Advocates (Silver)",
+			monthlyDollars: 100,
+			preset: tierPresets.xl,
+		},
+		{
+			title: "Patrons (Gold)",
+			monthlyDollars: 250,
+			preset: tierPresets.xl,
+		},
+	],
+});
